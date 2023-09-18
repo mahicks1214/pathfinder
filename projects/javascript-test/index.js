1ebc97c7d7ba7613162461e2ffe86c0c66816495
Michael Hickson <mhickson1214@gmail.com>
2023-09-18 07:28:42 -0700

    Monday Kickoff, need to fix faker syntax

---
+++ projects/javascript-test/index.js
@@ -0,0 +1,18 @@
+let a = 5;
+a = 12;
+
+// if (a == 5) {
+//    console.log('hello')
+// } else { 
+//    console.log('there')
+// }
+
+// const myTernary = a ===5 ? console.log('hello') : console.log('there'); // another way to work a if/else statemetn in JS
+
+let numbers = 0;
+
+for (let i = 0; i < 100; i++) {
+    if ( i % 3 ===0) { // % modulo is for division
+        numbers ++;
+    }
+}
